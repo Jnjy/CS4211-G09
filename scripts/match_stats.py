4625ec60db4467509710128a5cf63fa9299d0c40
Hau Chong <56034480+hauchongtang@users.noreply.github.com>
2023-10-02 14:24:16 +0800

    Add lineup parser and organize fields

--- scripts/match_stats.py
+++ scripts/match_stats.py
@@ -10,12 +10,34 @@ def get_match_stats_by_id(file_path, match_id):
   away_sofifa_ids = list(int(float(x)) for x in df['away_xi_sofifa_ids'].values[0].split(','))
   return home_sofifa_ids, away_sofifa_ids
 
+def group_players_by_lineup(file_path, match_id):
+  df = pd.read_csv(file_path)
+  url = f'https://www.premierleague.com/match/{match_id}'
+  df = df[df['match_url'] == url]
+  home_line_up, away_line_up = process_lineup(df['home_formation'].values[0]), process_lineup(df['away_formation'].values[0])
+  print(home_line_up, away_line_up)
+  return home_line_up, away_line_up
+
+def process_lineup(lineup):
+  if len(lineup.split('-')) != 1:
+    return lineup.split('-')
+  else:
+    result = []
+    arr = lineup.split('/')
+    result += list(int(x) for x in arr[:-1])
+    result.append(int(arr[-1]) % 1000)
+    return result
+
 # Run from project root
 # python ./scripts/match_stats.py ./datasets/matches/epl_matches_20152016.csv 12115
 def __main__():
   arguments = sys.argv[1:]
   home_ids, away_ids = get_match_stats_by_id(arguments[0], int(arguments[1]))
-  _, _ = player_stats.get_player_stats_by_ids("20152016", home_ids, is_away_team=False), player_stats.get_player_stats_by_ids("20152016", away_ids, is_away_team=True)
+  h_lineup, a_lineup = group_players_by_lineup(arguments[0], int(arguments[1]))
+  
+  # Start getting player stats
+  player_stats.get_player_stats_by_ids("20152016", home_ids, is_away_team=False, lineup=h_lineup)
+  player_stats.get_player_stats_by_ids("20152016", away_ids, is_away_team=True, lineup=a_lineup)
 
 if __name__ == '__main__':
   __main__()  
--- scripts/player_stats.py
+++ scripts/player_stats.py
@@ -1,36 +1,64 @@
 import sys
 import pandas as pd
+import player_enum
 
 def get_player_stats_by_id(file_path, player_id):
   df = pd.read_csv(file_path)
   df = df[df['sofifa_id'] == player_id]
   return df
 
-def get_player_stats_by_ids(year="20152016", player_ids=[], is_away_team=False):
+def get_player_stats_by_ids(year="20152016", player_ids=[], is_away_team=False, lineup=[4, 2, 3, 1]):
   file_path=f"./datasets/ratings/epl_ratings_{year}.csv"
   target_file = "./data_output/match_player_stats_away.txt" if is_away_team else "./data_output/match_player_stats_home.txt"
   with open(target_file, "w") as file:
     sys.stdout = file
-    for player_id in player_ids:
-      output_player_stats(get_player_stats_by_id(file_path, player_id))
+    for idx, player_id in enumerate(player_ids):
+      if idx == 0:
+        output_player_stats(get_player_stats_by_id(file_path, player_id), player_enum.GOALKEEPER)
+      elif 1 <= idx <= lineup[0]:
+        output_player_stats(get_player_stats_by_id(file_path, player_id), player_enum.DEFENDER)
+      elif lineup[0] < idx <= lineup[0] + lineup[1]:
+        output_player_stats(get_player_stats_by_id(file_path, player_id), player_enum.MIDFIELDER)
+      elif lineup[0] + lineup[1] < idx <= lineup[0] + lineup[1] + lineup[2]:
+        output_player_stats(get_player_stats_by_id(file_path, player_id), player_enum.FORWARD)
     
     sys.stdout = sys.__stdout__
     
 # For() takes in r_closeShot, r_longShot, r_volley, r_header, r_tackled, r_dribble
-def output_player_stats(df):
+def output_player_stats(df, player_type: int):
+  print(f"###### Player Type: {player_enum.player_output_mapping.get(player_type)}  ######")
   print("Player Name -> ", df['long_name'].values[0])
   print("Club Name -> ", df['club_name'].values[0])
+  # print("Short Pass -> ", df['attacking_short_passing'].values[0])
+  # print("Long Pass -> ", df['skill_long_passing'].values[0])
+  # print("Dribble -> ", df['skill_dribbling'].values[0])
+  # print("Attacking Crossing -> ", df['attacking_crossing'].values[0])
+  print("Attacking FK Accuracy -> ", df['skill_fk_accuracy'].values[0])
+  
+  # r_shortPass, r_longPass, r_tackled, r_dribble
+  print("\n###### Defenders() ######")
   print("Short Pass -> ", df['attacking_short_passing'].values[0])
   print("Long Pass -> ", df['skill_long_passing'].values[0])
+  print("Tackling -> ", df['defending_sliding_tackle'].values[0], " **This should be weighted tackle metrics, ignore for now!")
   print("Dribble -> ", df['skill_dribbling'].values[0])
+  
+  # r_shortPass, r_longPass, r_longShot, r_tackled, r_cross, r_dribble
+  print("\n###### MidFielders() ######")
+  print("Short Pass -> ", df['attacking_short_passing'].values[0])
+  print("Long Pass -> ", df['skill_long_passing'].values[0])
+  print("Power Long Shot -> ", df['power_long_shots'].values[0])
+  print("Tackling -> ", df['defending_sliding_tackle'].values[0], " **This should be weighted tackle metrics, ignore for now!")
   print("Attacking Crossing -> ", df['attacking_crossing'].values[0])
-  print("Attacking FK Accuracy -> ", df['skill_fk_accuracy'].values[0])
-  # r_closeShot, r_longShot, r_volley, r_header,
+  print("Dribble -> ", df['skill_dribbling'].values[0])
+  
+  # r_closeShot, r_longShot, r_volley, r_header, r_tackled, r_dribble
   print("\n###### Forwards() ######")
   print("Attacking Finishing -> ", df['attacking_finishing'].values[0])
   print("Power Long Shot -> ", df['power_long_shots'].values[0])
   print("Attacking Volleys -> ", df['attacking_volleys'].values[0])
   print("Attacking Heading -> ", df['attacking_heading_accuracy'].values[0])
+  print("Tackling -> ", df['defending_sliding_tackle'].values[0], " **This should be weighted tackle metrics, ignore for now!")
+  print("Dribble -> ", df['skill_dribbling'].values[0])
   
   print("\n###### Goal Keeping Stats ######")
   print("Goal Keeping Diving -> ", df['goalkeeping_diving'].values[0])
